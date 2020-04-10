0f2d830aceb5e19e09739d868f88a8ce3123e01c
Augusto Goncalves <augusto.goncalves@autodesk.com>
2020-04-09 17:46:23 -0700

    fix git structure

---
+++ public/js/ForgeViewer.js
@@ -0,0 +1,46 @@
+var viewer;
+
+// @urn the model to show
+// @viewablesId which viewables to show, applies to BIM 360 Plans folder
+function launchViewer(urn, viewableId, multiple) {
+  var options = {
+    env: 'AutodeskProduction',
+    getAccessToken: getForgeToken,
+    api: 'derivativeV2' + (atob(urn.replace('_', '/')).indexOf('emea') > -1 ? '_EU' : '') // handle BIM 360 US and EU regions
+  }
+
+  var documentId = 'urn:' + urn;
+  if (multiple && (viewer !== undefined)) {  // load additional models
+    loadDocument(documentId); 
+  }
+  else { // start viewer and load first model
+    Autodesk.Viewing.Initializer(options, () => {
+      viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: ['Autodesk.DocumentBrowser', 'Autodesk.VisualClusters'] });
+      viewer.start();
+      loadDocument(documentId);
+    });
+  }
+
+  function loadDocument(documentId) {
+    Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+  }
+
+  function onDocumentLoadSuccess(doc) {
+    var viewables = (viewableId ? doc.getRoot().findByGuid(viewableId) : doc.getRoot().getDefaultGeometry());
+    viewer.loadDocumentNode(doc, viewables, { keepCurrentModels: multiple }).then(i => {
+      // any additional action here?
+    });
+  }
+
+  function onDocumentLoadFailure(viewerErrorCode) {
+    console.error('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
+  }
+}
+
+function getForgeToken(callback) {
+  fetch('/api/forge/oauth/token').then(res => {
+    res.json().then(data => {
+      callback(data.access_token, data.expires_in);
+    });
+  });
+}
